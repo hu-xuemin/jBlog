8c5c81e5558cc09eeba64164a2db6ea699ed83c9
huxuemin <hu-xuemin@qq.com>
2017-11-13 00:53:29 +0800

    第一次提交。

---
+++ src/main/java/com/hxz/jblog/web/ContextManager.java
@@ -0,0 +1,18 @@
+package com.hxz.jblog.web;
+
+import javax.servlet.ServletContextEvent;
+
+
+public class ContextManager implements javax.servlet.ServletContextListener{
+
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+	}
+
+}
